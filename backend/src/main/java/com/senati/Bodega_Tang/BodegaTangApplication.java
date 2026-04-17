9642b6d48105dfa9d423aa1fd225d73cb07d12d0
emiaj0978 <vjgagoq.23@gmail.com>
2026-04-16 22:34:59 -0500

    asddff

---
+++ backend/src/main/java/com/senati/Bodega_Tang/BodegaTangApplication.java
@@ -0,0 +1,13 @@
+package com.senati.Bodega_Tang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodegaTangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodegaTangApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/senati/Bodega_Tang/BodegaTangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senati.Bodega_Tang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodegaTangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
